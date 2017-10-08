6c059b5dacebab4f6a9775d986894d662e563653
Pedro Oliveira <pedro.oliveira20@gmail.com>
2017-10-08 05:04:45 -0400

    Improve Tests

--- src/test/java/br/com/ppm/commons/ToStringBuilderTest.java
+++ src/test/java/br/com/ppm/commons/ToStringBuilderTest.java
@@ -1,4 +1,4 @@
-/* 
+/*
  * Copyright (C) 2017 pedrotoliveira
  *
  * This program is free software: you can redistribute it and/or modify
@@ -16,7 +16,6 @@
  */
 package br.com.ppm.commons;
 
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -30,11 +29,10 @@ import br.com.ppm.commons.model.ClassB;
 import br.com.ppm.commons.model.Order;
 import br.com.ppm.commons.model.Person;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.equalTo;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.*;
 
 /**
  * The Class ToStringBuilderTest.
@@ -44,39 +42,38 @@ import static org.junit.Assert.assertThat;
 public class ToStringBuilderTest {
 
     /**
-   	 * Test method for.
-	 *
-	 * {@link com.ppm..util.ToStringBuilder#reflectionToString(java.lang.Object)}
-	 * .
-	 */
-	@Test
-	public final void testReflectionToString() {
-		Person person = new Person();
-		person.setName("John Doe");
-		person.setAddress(new Address("Some street", 35, "Apto 2"));
-		person.setAge(35);
-		person.setAlive(true);
-		person.setNumbers(new short[]{1, 3, 4, 5});
-		person.setOrders(Arrays.asList(new Order[]{new Order(12358), new Order(12387), new Order(821)}));
+     * Test method for.
+     *
+     * {@link com.ppm..util.ToStringBuilder#reflectionToString(java.lang.Object)} .
+     */
+    @Test
+    public final void testReflectionToString() {
+        Person person = new Person();
+        person.setName("John Doe");
+        person.setAddress(new Address("Some street", 35, "Apto 2"));
+        person.setAge(35);
+        person.setAlive(true);
+        person.setNumbers(new short[]{1, 3, 4, 5});
+        person.setOrders(Arrays.asList(new Order[]{new Order(12358), new Order(12387), new Order(821)}));
 
-		Map<String, Person> parents = new HashMap<>();
-		Person father = new Person();
-		father.setName("Bill Doe");
-		father.setAddress(new Address("Cemitery", 582, null));
-		father.setAge(0);
-		father.setAlive(false);
-		father.setNumbers(null);
-		parents.put("father", father);
+        Map<String, Person> parents = new HashMap<>();
+        Person father = new Person();
+        father.setName("Bill Doe");
+        father.setAddress(new Address("Cemitery", 582, null));
+        father.setAge(0);
+        father.setAlive(false);
+        father.setNumbers(null);
+        parents.put("father", father);
 
-		Person mother = new Person();
-		mother.setName("Willy Mae Doe");
-		mother.setAddress(new Address("Older street", 82, "bloco a"));
-		mother.setAge(70);
-		mother.setAlive(false);
-		mother.setNumbers(new short[]{9, 7, 6, 8});
-		parents.put("mother", mother);
+        Person mother = new Person();
+        mother.setName("Willy Mae Doe");
+        mother.setAddress(new Address("Older street", 82, "bloco a"));
+        mother.setAge(70);
+        mother.setAlive(false);
+        mother.setNumbers(new short[]{9, 7, 6, 8});
+        parents.put("mother", mother);
 
-		person.setParents(parents);
+        person.setParents(parents);
 
         String toStringExpected = "Person[name=John Doe, age=35, "
                 + "address=Address[street=Some street, number=35, complement=Apto 2], "
@@ -94,63 +91,93 @@ public class ToStringBuilderTest {
 
         String result = person.toString();
         assertThat("toString result should be equal to expected", result, equalTo(toStringExpected));
-	}
+    }
 
     @Test
-	public void testCyclicReferences() {
+    public void testCyclicReferences() {
         ClassA a = new ClassA();
         ClassB b = new ClassB(a);
-		a.setB(b);
+        a.setB(b);
 
-		String result = ToStringBuilder.reflectionToString(b);
-		System.out.println(result);
-	}
+        String result = ToStringBuilder.reflectionToString(b);
+        String errorExpected = "Error on ToStringBuilder, reflectionToString method has reached the max depth calls!"
+                + " Please verify the circular references";
 
-	@Test
-	public void testMaskFields() {
-		String number = "1058030000000055";
-		String cvv = "***";
-		String maskedNumber = "105803******0055";
-		String expected = "Card[ccNumber=" + maskedNumber + ", cvv=" + "***" + "]";
+        assertTrue("Result should contain the error text", result.contains(errorExpected));
+    }
+
+    @Test
+    public void testMaskFields() {
+        String number = "1058030000000055";
+        String cvv = "***";
+        String maskedNumber = "105803******0055";
+        String expected = "Card[ccNumber=" + maskedNumber + ", cvv=" + "***" + "]";
         String result = new Card(number, cvv).toString();
         assertThat("CardNumber should be masked", result, equalTo(expected));
-	}
+    }
 
-	/**
-	 * Test collections.
-	 */
-	@Test
-	public void testCollections() {
-		final List<String> l = Arrays.asList(new String[]{"one", "two", "three"});
-		final List<List<String>> listWithList = new ArrayList<>();
-		listWithList.add(l);
-		listWithList.add(l);
-		listWithList.add(l);
+    /**
+     * Test collections.
+     */
+    @Test
+    public void testCollections() {
+        final List<String> l = Arrays.asList(new String[]{"one", "two", "three"});
+        final List<List<String>> listWithList = new ArrayList<>();
+        listWithList.add(l);
+        listWithList.add(l);
+        listWithList.add(l);
 
-		String toStringExpected = "ArrayList{"
-				+ " ArrayList{ [one], [two], [three] },"
-				+ " ArrayList{ [one], [two], [three] },"
-				+ " ArrayList{ [one], [two], [three] }"
-				+ " }";
+        String toStringExpected = "ArrayList{"
+                + " ArrayList{ [one], [two], [three] },"
+                + " ArrayList{ [one], [two], [three] },"
+                + " ArrayList{ [one], [two], [three] }"
+                + " }";
 
         String result = ToStringBuilder.reflectionToString(listWithList);
         assertThat("toString of Collections should be equal to expected", result, equalTo(toStringExpected));
-	}
+    }
 
     @Test
-	public void testArrays() {
+    public void testArrays() {
+        String[] stringArray = new String[]{"one", "two", "three"};
+        String result = ToStringBuilder.reflectionToString(stringArray);
+        String expected = "[one, two, three]";
+        assertThat("toString of Array should be equal to expected", result, equalTo(expected));
+    }
 
-	}
+    @Test
+    public void testWrapperLong() {
+        Long teraFlops = 1093291043094920349L;
+        String result = new ToStringBuilder(teraFlops).build();
+        assertThat("toString shoud be equal to [1093291043094920349]", result, equalTo("[1093291043094920349]"));
+    }
 
-	@Test
-	@Ignore("Teste a ser Implementado.")
-	public void testWrappers() {
-		//TODO: Implementar!!
-	}
+    @Test
+    public void testWrapperInteger() {
+        Integer one = 1;
+        assertThat("toString shoud be equal to [1]", new ToStringBuilder(one).build(true), equalTo("[1]"));
+    }
 
-	@Test
-	@Ignore("Teste a ser Implementado.")
-	public void testMap() {
-		//TODO: Implementar!!
-	}
+    @Test
+    public void testWrapperDouble() {
+        Double half = 0.5d;
+        assertThat("toString shoud be equal to [0.5]", new ToStringBuilder(half).build(false), equalTo("[0.5]"));
+    }
+
+    @Test
+    public void testMap() {
+        Map<String, Integer> map = new HashMap<>();
+        map.put("one", 1);
+        map.put("two", 2);
+        map.put("three", 3);
+
+        String result = ToStringBuilder.reflectionToString(map);
+        String expected = "Map{ (k1=one, v1=[1]), (k2=two, v2=[2]), (k3=three, v3=[3]) }";
+        assertThat("toString shoud be equal to expected", result, equalTo(expected));
+    }
+
+    @Test
+    public void testNull() {
+        assertThat("result should be Object=null ", ToStringBuilder.reflectionToString(null), equalTo("Object=null "));
+    }
 }
