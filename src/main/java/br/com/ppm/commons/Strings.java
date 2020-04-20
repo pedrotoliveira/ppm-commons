5126504e9defb56228c01df19e5b2d484c1f39fe
pedrotoliveira <pedro.oliveira20@gmail.com>
2020-04-19 23:39:34 -0300

    Refactoring

--- src/main/java/br/com/ppm/commons/Strings.java
+++ src/main/java/br/com/ppm/commons/Strings.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017 pedrotoliveira
+ * Copyright (C) 2020 pedrotoliveira
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -27,6 +27,7 @@ public final class Strings {
      * No instances for this class
      */
     private Strings() {
+        throw new AssertionError("No instances for you!");
     }
 
     /**
@@ -37,7 +38,7 @@ public final class Strings {
      * @return the new text with first letter Capitalized
      */
     public static String capitalizeFirstLetter(final String text) {
-        return String.valueOf(Character.toUpperCase(text.charAt(0))) +
+        return Character.toUpperCase(text.charAt(0)) +
                 text.substring(1);
     }
 
@@ -72,6 +73,6 @@ public final class Strings {
         for (int i = 0; i < maskSize; i++) {
             sb.append('*');
         }
-        return sb.append(value.substring(size - lastPart, size)).toString();
+        return sb.append(value, size - lastPart, size).toString();
     }
 }
