a6c0699e96455437a0d79f133028be07396eadbf
pedrotoliveira <pedro.oliveira20@gmail.com>
2020-04-21 05:25:29 -0300

    Working on Issues [#31, #36, #37]

--- src/main/java/br/com/ppm/commons/array/ArraysToStringBuilder.java
+++ src/main/java/br/com/ppm/commons/array/ArraysToStringBuilder.java
@@ -38,6 +38,14 @@ public final class ArraysToStringBuilder implements ToStringBuilder {
 
     @Override
     public String build(boolean ignoreSuperType, ToStringStyle.Style style) {
+        KeyValueAppender appender = appendValues(style);
+        if (array.length > PAGE_SIZE) {
+            appender.appendSeparator("...");
+        }
+        return appender.appendTerminator(CLOSE_SQUARE_BRACKET);
+    }
+
+    private KeyValueAppender appendValues(ToStringStyle.Style style) {
         KeyValueAppender appender = KeyValueAppender.start(OPEN_SQUARE_BRACKET);
         for (int index = 0; index < array.length; index++) {
             appender.appendValue(array[index], style);
@@ -49,9 +57,6 @@ public final class ArraysToStringBuilder implements ToStringBuilder {
                 appender.appendSeparator(COMMA);
             }
         }
-        if (array.length > PAGE_SIZE) {
-            appender.appendSeparator("...");
-        }
-        return appender.appendTerminator(CLOSE_SQUARE_BRACKET);
+        return appender;
     }
 }
